a2d28bd2af6a8d90c323e3968c627878e76db176
Aaron-Speedy <aaron@lspeedy.org>
2025-08-12 13:45:31 -0500

    Create window

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -xe
 CFLAGS="-Wall -Wextra -ggdb -O3 -std=gnu11"
 
 (cd libwebp && make -f makefile.unix)
-cc wallpaper.c -o wallpaper $CFLAGS "libwebp/src/libwebp.a"
+cc wallpaper.c -o wallpaper $CFLAGS "libwebp/src/libwebp.a" -L/usr/X11R6/lib -lX11
--- wallpaper.c
+++ wallpaper.c
@@ -3,6 +3,8 @@
 
 #include "libwebp/src/webp/decode.h"
 
+#include <X11/Xlib.h>
+
 int main() {
     Arena perm = new_arena(1 * GiB);
     Arena scratch = new_arena(1 * KiB);
@@ -10,11 +12,40 @@ int main() {
     s8 data = read_file(&perm, scratch, s8("150.webp"));
     printf("%ld\n", data.len);
 
-    int w, h;
-    WebPGetInfo(data.buf, data.len, &w, &h);
-    printf("%dx%d\n", w, h);
+    int width, height;
+    WebPGetInfo(data.buf, data.len, &width, &height);
+    printf("%dx%d\n", width, height);
 
-    u8 *pixels = WebPDecodeRGB(data.buf, data.len, &w, &h);
+    u8 *pixels = WebPDecodeRGB(data.buf, data.len, &width, &height);
+
+    Display *d;
+    Window w;
+    XEvent e;
+    const char *msg = "Hello, World!";
+    int s;
+
+    d = XOpenDisplay(NULL);
+    if (d == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
+    }
+
+    s = DefaultScreen(d);
+    w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1, BlackPixel(d, s), WhitePixel(d, s));
+    XSelectInput(d, w, ExposureMask | KeyPressMask);
+    XMapWindow(d, w);
+
+    while (1) {
+        XNextEvent(d, &e);
+        if (e.type == Expose) {
+            XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
+            XDrawString(d, w, DefaultGC(d, s), 10, 50, msg, strlen(msg));
+        }
+        if (e.type == KeyPress)
+            break;
+    }
+
+    XCloseDisplay(d);
 
     return 0;
 }
