fb6a24d8d41f88122bef27a606b8936fffdc529f
Aaron-Speedy <aaron@lspeedy.org>
2025-08-20 13:02:08 -0500

    Fix issues with multithreading

--- src/wallpaper.c
+++ src/wallpaper.c
@@ -30,12 +30,12 @@ typedef struct {
 } S8ArenaPair;
 
 typedef struct {
-    bool lock;
     bool redraw;
     Image img;
 } Background;
 
 Background background = {0};
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 size_t curl_get_data(char *buf, size_t item_len, size_t item_num, void *data) {
     size_t n = item_len * item_num;
@@ -66,77 +66,77 @@ s8 download(Arena *perm, CURL *curl, s8 url) {
 }
 
 void *background_thread() {
-    int timeout_s = 10;
-
-    Arena frame = new_arena(1 * GiB);
+    Arena perm = new_arena(1 * GiB);
 
     CURL *curl = curl_easy_init(); 
     if (!curl) err("Failed to initialize libcurl.");
 
+    int timeout_s = 60;
+
     bool initial = true;
+    pthread_mutex_lock(&lock); // initial lock
 
     while (true) {
+        Arena scratch = perm;
+
         time_t a_time = time(NULL);
         u8 *buf = NULL;
         Image img = {0};
 
-        {
-            Arena scratch = frame;
+        s8 base = s8("https://infotoast.org/images/");
 
-            s8 base = s8("https://infotoast.org/images/");
+        u64 n = s8_to_u64(download(
+            &scratch,
+            curl,
+            s8_newcat(&scratch, base, s8("num.txt"))
+        ));
 
-            u64 n = s8_to_u64(download(
-                &scratch,
-                curl,
-                s8_newcat(&scratch, base, s8("num.txt"))
-            ));
+        s8 name = u64_to_s8(&scratch, rand() % (n + 1), 0);
+        s8_modcat(&scratch, &name, s8(".webp"));
 
-            s8 name = u64_to_s8(&scratch, rand() % (n + 1), 0);
-            s8_modcat(&scratch, &name, s8(".webp"));
+        s8 data = download(
+            &scratch,
+            curl,
+            s8_newcat(&scratch, base, name)
+        );
 
-            s8 data = download(
-                &scratch,
-                curl,
-                s8_newcat(&scratch, base, name)
-            );
+        WebPGetInfo(data.buf, data.len, &img.w, &img.h);
+        printf("%dx%d\n", img.w, img.h);
+        buf = WebPDecodeRGB(data.buf, data.len, &img.w, &img.h);
 
-            WebPGetInfo(data.buf, data.len, &img.w, &img.h);
-            printf("%dx%d\n", img.w, img.h);
-            buf = WebPDecodeRGB(data.buf, data.len, &img.w, &img.h);
+        img = new_img(&scratch, img);
+
+        for (int x = 0; x < img.w; x++) {
+            for (int y = 0; y < img.h; y++) {
+                int i = img_at(img, x, y);
+                img.buf[i].c[0] = buf[i * 3 + 0];
+                img.buf[i].c[1] = buf[i * 3 + 1];
+                img.buf[i].c[2] = buf[i * 3 + 2];
+                pack_color(img, x, y);
+            }
         }
+
+        WebPFree(buf);
+
+        Image b = rescale_img(
+            NULL,
+            img,
+            background.img.w,
+            background.img.h
+        );
 
         int wait = timeout_s - (time(NULL) - a_time);
         if (wait > 0 && !initial) sleep(wait);
 
-        while (background.lock) {}
+        if (!initial) pthread_mutex_lock(&lock); /* if it's on the first
+                                                    iteration, it's already
+                                                    locked. */
+            free(background.img.buf);
+            free(background.img.packed);
+            background.img = b;
+            background.redraw = true;
+        pthread_mutex_unlock(&lock);
 
-        background.lock = true;
-        {
-            frame.len = 0;
-
-            img = new_img(&frame, img);
-
-            for (int x = 0; x < img.w; x++) {
-                for (int y = 0; y < img.h; y++) {
-                    int i = img_at(img, x, y);
-                    img.buf[i].c[0] = buf[i * 3 + 0];
-                    img.buf[i].c[1] = buf[i * 3 + 1];
-                    img.buf[i].c[2] = buf[i * 3 + 2];
-                    pack_color(img, x, y);
-                }
-            }
-
-            WebPFree(buf);
-
-            background.img = rescale_img(
-                &frame,
-                img,
-                background.img.w,
-                background.img.h
-            );
-        }
-        background.lock = false;
-        background.redraw = true;
         initial = false;
     }
 
@@ -167,45 +167,38 @@ int main() {
     load_font(&font, win.dpi_x, win.dpi_y);
 
     float time_x = 0.1, time_y = 0.25;
-    int draw_timeout_ms = 1000, draw_init = 0;
     Image prev_bb = {0}; // previous text bounding box
 
     while (1) {
-        WinEvent event = next_event_timeout(&win, draw_init);
-        draw_init = draw_timeout_ms;
+        pthread_mutex_lock(&lock);
+        pthread_mutex_unlock(&lock);
 
-        if (!event.type) continue; // error
+        Arena scratch  = perm;
+        time_t ftime = time(NULL) + 1;
 
-        printf("%d\n", background.lock);
-        while (background.lock) {}
-        while (!background.img.buf) {}
+        struct tm *lt = localtime(&ftime);
 
-        background.lock = true;
-        {
-            Arena scratch  = perm;
-            time_t cur_time = time(NULL);
+        // TODO: remove the need to specify names and repeat the order
+        s8 hour = u64_to_s8(&scratch, lt->tm_hour, 2),
+           colon = s8_copy(&scratch, s8(":")),
+           minute = u64_to_s8(&scratch, lt->tm_min, 2),
+           colon2 = s8_copy(&scratch, s8(":")),
+           second = u64_to_s8(&scratch, lt->tm_sec, 2),
+           // space = s8_copy(&scratch, s8(" ")),
+           // ampm = s8_copy(&scratch, lt->tm_hour >= 12 ? s8("PM") : s8("AM")),
+           time_str = {
+            .buf = hour.buf,
+            .len = hour.len + colon.len +
+                   minute.len + colon2.len +
+                   second.len,
+        };
 
-            struct tm *lt = localtime(&cur_time);
-
-            // TODO: remove the need to specify names and repeat the order
-            s8 hour = u64_to_s8(&scratch, lt->tm_hour, 2),
-               colon = s8_copy(&scratch, s8(":")),
-               minute = u64_to_s8(&scratch, lt->tm_min, 2),
-               colon2 = s8_copy(&scratch, s8(":")),
-               second = u64_to_s8(&scratch, lt->tm_sec, 2),
-               // space = s8_copy(&scratch, s8(" ")),
-               // ampm = s8_copy(&scratch, lt->tm_hour >= 12 ? s8("PM") : s8("AM")),
-               time_str = {
-                .buf = hour.buf,
-                .len = hour.len + colon.len +
-                       minute.len + colon2.len +
-                       second.len,
-            };
-
+        pthread_mutex_lock(&lock);
             if (background.redraw) {
                 place_img(screen, background.img, 0.0, 0.0);
-                if (event.type == EVENT_TIMEOUT) background.redraw = false;
+                background.redraw = false;
             } else place_img(screen, prev_bb, prev_bb.x, prev_bb.y);
+
             prev_bb = draw_text(
                 screen,
                 &font,
@@ -215,8 +208,17 @@ int main() {
             );
             prev_bb.buf = background.img.buf;
             prev_bb.packed = background.img.packed;
-        }
-        background.lock = false;
+        pthread_mutex_unlock(&lock);
+
+        struct timeval time_val;
+        gettimeofday(&time_val, NULL);
+
+        WinEvent event = next_event_timeout(
+            &win,
+            1000 - (time_val.tv_usec / 1000) // update exactly on the second
+        );
+
+        if (!event.type) continue; // error
 
         draw_to_win(win);
     }
