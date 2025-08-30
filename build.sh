6b1952eea83e83bf3e4577d0481abdc707d01634
Aaron-Speedy <aaronspeedy@protonmail.com>
2025-08-30 01:06:40 -0500

    Add test.c to open window and draw to it

--- build.sh
+++ build.sh
@@ -8,23 +8,29 @@ OS=$(uname | tr '[:upper:]' '[:lower:]')
 
 LIBWEBP=""
 CURL=""
+WINDOWING=""
 
 if [[ "$OS" == "windows"* ]]; then
     LIBWEBP="third_party/real/libwebp/lib/libwebp.lib"
     CURL="third_party/real/curl/lib/libcurl.a"
+    WINDOWING="-mwindows"
 elif [[ "$OS" == "linux"* ]]; then
     LIBWEBP="third_party/real/libwebp/src/libwebp.a"
     CURL="-lcurl"
+    WINDOWING="-L/usr/X11R6/lib -lX11"
 else
     echo "Unknown platform. Exiting..."
     exit
 fi
 
 LIBS="\
-$LIBWEBP \
--L/usr/X11R6/lib -lX11 \
 -lm \
+$LIBWEBP \
+$WINDOWING \
 $CURL \
 "
 
-cc src/wallpaper.c -o wallpaper $CFLAGS $LIBS
+rm -rf build
+mkdir build
+# cc src/wallpaper.c -o build/wallpaper $CFLAGS $LIBS
+cc test.c -o build/test $CFLAGS $LIBS
--- configure.sh
+++ configure.sh
@@ -4,26 +4,26 @@ set -xe
 
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 
-webpwin="libwebp-1.6.0-windows-x64"
-webpurlwin="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/$webpwin.zip"
+WEBPWIN="libwebp-1.6.0-windows-x64"
+WEBPURLWIN="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/$WEBPWIN.zip"
 
-curlwin="curl-8.15.0_5-win64-mingw"
-curlurlwin="https://curl.se/windows/dl-8.15.0_5/$curlwin.zip"
+CURLWIN="curl-8.15.0_5-win64-mingw"
+CURLURLWIN="https://curl.se/windows/dl-8.15.0_5/$CURLWIN.zip"
 
 rm -rf third_party/real
 mkdir third_party/real
 cd third_party/real
 
 if [[ "$OS" == "windows"* ]]; then
-    curl $webpurlwin --output libwebp.zip
+    curl $WEBPURLWIN --output libwebp.zip
     unzip libwebp.zip
     rm libwebp.zip
-    mv $webpwin libwebp
+    mv $WEBPWIN libwebp
 
-    curl $curlurlwin --output curl.zip
+    curl $CURLURLWIN --output curl.zip
     unzip curl.zip
     rm curl.zip
-    mv $curlwin curl
+    mv $CURLWIN curl
 elif [[ "$OS" == "linux"* ]]; then
     cp -r ../libwebp .
     (cd libwebp && make -f makefile.unix)
