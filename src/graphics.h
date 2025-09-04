51167b2f219560df79a3a445c71d84bb2ff7b33d
Aaron-Speedy <aaronspeedy@protonmail.com>
2025-09-04 11:58:31 -0500

    Draw to the wallpaper
    Finally!!!

--- src/graphics.h
+++ src/graphics.h
@@ -35,6 +35,7 @@ typedef enum {
 typedef struct {
     HWND win;
     BITMAPINFO bitmap_info;
+    HWND worker_w, shell;
 } PlatformWin;
 #else
 #error "Unsupported platform!"
@@ -73,29 +74,64 @@ void close_win(Win win);
 
 #ifdef _WIN32
 
-LRESULT _main_win_cb(HWND pwin, UINT msg, WPARAM hv, LPARAM vv) {
-    LRESULT ret = 0;
+// https://www.codeproject.com/Articles/856020/Draw-Behind-Desktop-Icons-in-Windows-plus
 
+BOOL _find_worker_w_cb(HWND top, LPARAM vv) {
+    HWND p = FindWindowExA(top, 0, "SHELLDLL_DefView", 0);
+    if (p) {
+        PlatformWin *win = (PlatformWin *) vv;
+        win->worker_w = FindWindowExA(0, top, "WorkerW", 0);
+        win->shell = p;
+        return false;
+    }
+    return true;
+}
+
+void _find_worker_w(PlatformWin *win) {
+    HWND progman = FindWindowA("Progman", 0);
+    if (!progman) return;
+
+    SendMessageTimeoutA(progman, 0x052C, 0xD, 0x1, SMTO_NORMAL, 1000, 0);
+
+    EnumWindows(_find_worker_w_cb, (LPARAM) win);
+
+    // TODO: Other stuff for CONFIGURATIONNNNNNNNNNNNNNNNNNNNNNNNNNNN!!!!!!!!!!!!!! depending on the set up.
+
+    // p.worker_w = FindWindowExA(progman, 0, "WorkerW", 0);
+
+    // HWND shell = FindWindowExA(progman, 0, "SHELLDLL_DefView", 0);
+
+    // if (shell) {
+    //     assert(0);
+    //     worker_w = GetWindow(shell, GW_HWNDPREV);
+    //     char name[256] = {0};
+    //     GetClassName(worker_w, name, sizeof(name));
+    //     if (!strcmp(name, "WorkerW")) {
+    //         assert(0);
+    //         return false;
+    //     }
+    // }
+}
+
+LRESULT _main_win_cb(HWND pwin, UINT msg, WPARAM hv, LPARAM vv) {
     Win *win = (Win *) GetWindowLongPtr(pwin, GWLP_USERDATA);
+
+    // If the window isn't passed, do the default window procedure.
+    if (!win) {
+        return DefWindowProc(pwin, msg, hv, vv);
+    }
 
     RECT client_rect;
     GetClientRect(pwin, &client_rect);
 
-    if (win) {
-        win->p.win = pwin;
-        win->w = client_rect.right - client_rect.left;
-        win->h = client_rect.bottom - client_rect.top;
-        win->dpi_x = win->dpi_y = GetDpiForWindow(pwin);
-    }
+    win->p.win = pwin;
+    win->w = client_rect.right - client_rect.left;
+    win->h = client_rect.bottom - client_rect.top;
+    win->dpi_x = win->dpi_y = GetDpiForWindow(pwin);
     
     switch (msg) {
-        case WM_TIMER: {
-            assert(win);
-            win->event = EVENT_TIMEOUT;
-        } break;
+        case WM_TIMER: win->event = EVENT_TIMEOUT; break;
         case WM_SIZE: {
-            assert(win);
-
             if (win->buf) {
                 free(win->buf);
             }
@@ -114,7 +150,6 @@ LRESULT _main_win_cb(HWND pwin, UINT msg, WPARAM hv, LPARAM vv) {
             win->resized = true;
         } break;
         case WM_DESTROY: case WM_CLOSE: {
-            assert(win);
             win->event = EVENT_QUIT;
             PostQuitMessage(0);
         } break;
@@ -125,11 +160,11 @@ LRESULT _main_win_cb(HWND pwin, UINT msg, WPARAM hv, LPARAM vv) {
             EndPaint(pwin, &paint);
         } break;
         default: {
-            ret = DefWindowProc(pwin, msg, hv, vv);
+            return DefWindowProc(pwin, msg, hv, vv);
         } break;
     }
 
-    return ret;
+    return 0;
 }
 #endif
 
@@ -139,8 +174,8 @@ void get_bg_win(Win *win) {
     *win = (Win) {0};
 
 #ifdef __linux__
-    win->p.display = XOpenDisplay(NULL);
-    if (win->p.display == NULL) err("Failed to open display.");
+    win->p.display = XOpenDisplay(0);
+    if (win->p.display == 0) err("Failed to open display.");
 
     win->p.screen = DefaultScreen(win->p.display);
     win->p.win = DefaultRootWindow(win->p.display);
@@ -164,41 +199,41 @@ void get_bg_win(Win *win) {
         24, ZPixmap, 0, (char *) buf, w, h, 32, 0
     );
 
-    if (win->p.img == NULL) err("Failed to create window.");
+    if (win->p.img == 0) err("Failed to create window.");
 
 #elif _WIN32
     WNDCLASS win_class = {
         .style = CS_HREDRAW | CS_VREDRAW,
         .lpfnWndProc = _main_win_cb,
-        .hInstance = GetModuleHandle(NULL),
+        .hInstance = GetModuleHandle(0),
         .lpszClassName = "wallpaperworks",
-       	.hCursor = LoadCursor(NULL, IDC_ARROW),
+       	.hCursor = LoadCursor(0, IDC_ARROW),
     };
     RegisterClassA(&win_class); // TODO: Check for errors.
+
+    _find_worker_w(&win->p);
 
     win->p.win = CreateWindowExA( // TODO: Check for errors.
         0,
         win_class.lpszClassName,
-        "Untited Window\n", // TODO: Add ability to change name
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        "Untited Window", // TODO: Add ability to change name
+        WS_POPUP | WS_BORDER | WS_SYSMENU | WS_MAXIMIZE,
+        0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
         0,
         0,
-        GetModuleHandle(NULL),
+        GetModuleHandle(0),
         0
     );
 
+    // SetWindowLong(win->p.win, GWL_STYLE, 0);
     SetWindowLongPtr(win->p.win, GWLP_USERDATA, (LONG_PTR) win);
+    SetParent(win->p.win, win->p.worker_w);
 
-    RECT client_rect;
-    GetClientRect(win->p.win, &client_rect);
-    win->w = client_rect.right - client_rect.left;
-    win->h = client_rect.bottom - client_rect.top;
+    RECT rect;
+    GetClientRect(win->p.win, &rect);
+    win->w = rect.right - rect.left;
+    win->h = rect.bottom - rect.top;
     win->dpi_x = win->dpi_y = GetDpiForWindow(win->p.win);
-
     win->buf = calloc(win->w * win->h, sizeof(*win->buf));
 
     ShowWindow(win->p.win, SW_NORMAL);
@@ -242,7 +277,7 @@ void next_event_timeout(Win *win, int timeout_ms) {
     tv.tv_sec = timeout_ms / 1000;
     tv.tv_usec = (timeout_ms % 1000) * 1000;
 
-    int select_ret = select(x11_fd + 1, &fds, NULL, NULL, &tv);
+    int select_ret = select(x11_fd + 1, &fds, 0, 0, &tv);
 
     XEvent e;
     if (select_ret > 0) {
@@ -255,7 +290,7 @@ void next_event_timeout(Win *win, int timeout_ms) {
         win->event = EVENT_TIMEOUT;
     }
 #elif _WIN32
-    UINT_PTR timer_id = SetTimer(win->p.win, 1, timeout_ms, NULL);
+    UINT_PTR timer_id = SetTimer(win->p.win, 1, timeout_ms, 0);
     if (!timer_id) {
         win->event = EVENT_ERR;
         return;
@@ -267,7 +302,7 @@ void next_event_timeout(Win *win, int timeout_ms) {
         DispatchMessage(&msg);
     } else win->event = EVENT_ERR;
 
-    KillTimer(NULL, timer_id);
+    KillTimer(0, timer_id);
 #endif
 }
 
--- test.c
+++ test.c
@@ -1,14 +1,22 @@
+#include <stdio.h>
+
 #define err(...) do { \
-  fprintf(stderr, "Error: "); \
-  fprintf(stderr, __VA_ARGS__); \
-  fprintf(stderr, "\n"); \
-  exit(1); \
+    fprintf(stderr, "Error: "); \
+    fprintf(stderr, __VA_ARGS__); \
+    fprintf(stderr, "\n"); \
+    exit(1); \
 } while (0);
 
 #define warning(...) do { \
-  fprintf(stderr, "Warning: "); \
-  fprintf(stderr, __VA_ARGS__); \
-  fprintf(stderr, "\n"); \
+    fprintf(stderr, "Warning: "); \
+    fprintf(stderr, __VA_ARGS__); \
+    fprintf(stderr, "\n"); \
+} while (0);
+
+#define print(format, ...) do { \
+    char buf[1024]; \
+    _snprintf_s(buf, sizeof(buf), _TRUNCATE, format, __VA_ARGS__); \
+    MessageBox(NULL, buf, "Debug Message", MB_OK); \
 } while (0);
 
 #define GRAPHICS_IMPL
@@ -24,30 +32,31 @@ int main() {
     Win win = {0};
     get_bg_win(&win);
 
-    FFont font = {
-        .path = "./resources/Mallory/Mallory Medium.ttf",
-        .pt = 30,
-    };
-    load_font(&font, win.dpi_x, win.dpi_y);
+    // FFont font = {
+    //     .path = "./resources/Mallory/Mallory Medium.ttf",
+    //     .pt = 30,
+    // };
+    // load_font(&font, win.dpi_x, win.dpi_y);
 
     Color color = { .c[COLOR_B] = 255, };
 
     while (1) {
         Image screen = { .buf = win.buf, .w = win.w, .h = win.h, .alloc_w = win.w, };
 
+
         for (int i = 0; i < screen.w * screen.h; i++) {
             screen.buf[i] = (i % 100 >= 50) ? color : (Color) {0};
         }
 
 
-        draw_text(
-            screen,
-            &font,
-            s8("Hi. Watch me guys."),
-            screen.w * 0.5,
-            screen.h * 0.5,
-            255, 255, 255
-        );
+        // draw_text(
+        //     screen,
+        //     &font,
+        //     s8("Hi. Watch me guys."),
+        //     screen.w * 0.5,
+        //     screen.h * 0.5,
+        //     255, 255, 255
+        // );
 
         draw_to_win(win);
 
