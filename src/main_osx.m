cc4b8151edde2d3ad436e9eebe86f23d2a998c97
Aaron-Speedy <aaron@lspeedy.org>
2025-11-03 09:37:11 -0600

    Upload

--- src/main_osx.m
+++ src/main_osx.m
@@ -1,90 +1,25 @@
-#import <Cocoa/Cocoa.h>
+#include <Cocoa/Cocoa.h>
 
-@interface CircleView : NSView
-@end
-
-@implementation CircleView
-
--(BOOL) isOpaque {
-    return NO;
-}
-
--(void) drawRect : (NSRect) dirtyRect {
-    [super drawRect:dirtyRect];
-
-    NSRect bounds = [self bounds];
-    
-    CGFloat padding = 0.0;
-    CGFloat minDim = MIN(bounds.size.width, bounds.size.height) - (2 * padding);
-    
-    NSRect circleRect = NSMakeRect(
-        bounds.origin.x + (bounds.size.width - minDim) / 2.0,
-        bounds.origin.y + (bounds.size.height - minDim) / 2.0,
-        minDim,
-        minDim
-    );
-
-    NSBezierPath *circlePath = [NSBezierPath bezierPathWithOvalInRect:circleRect];
-    
-    [[NSColor systemBlueColor] setFill];
-    [circlePath fill]; 
-    
-    [[NSColor darkGrayColor] setStroke];
-    [circlePath setLineWidth:3.0];
-    [circlePath stroke];
-}
-
-@end
-
-@interface AppDelegate : NSObject <NSApplicationDelegate>
-@property (nonatomic, strong) NSWindow *window;
-@end
-
-@implementation AppDelegate
-
--(void) applicationDidFinishLaunching : (NSNotification *) aNotification {
-    // 1. Create the window frame and style
-    NSRect frame = NSMakeRect(200, 200, 300, 300);
-    NSUInteger styleMask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable;
-    
-    // 2. Initialize the NSWindow. NSBackingStoreBuffered ensures drawing uses a buffer.
-    self.window = [[NSWindow alloc] initWithContentRect:frame
-                                             styleMask:styleMask
-                                               backing:NSBackingStoreBuffered 
-                                                 defer:NO];
-    
-    [self.window setTitle:@"Title of Window"];
-    [self.window setMinSize:NSMakeSize(150, 150)];
-    
-    // 3. Create and configure the custom drawing view
-    CircleView *circleView = [[CircleView alloc] initWithFrame:[[self.window contentView] bounds]];
-    // Ensure the circle resizes when the window resizes
-    [circleView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-    
-    // 4. Set the custom view as the window's content view
-    [self.window setContentView:circleView];
-    
-    // 5. Display the window
-    [self.window makeKeyAndOrderFront:nil];
-}
-
--(BOOL) applicationShouldTerminateAfterLastWindowClosed : (NSApplication *) sender {
-    return YES;
-}
-
-@end
-
-
-// --- Main Function (The Entry Point) ---
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        NSApplication *application = [NSApplication sharedApplication];
-        
-        AppDelegate *delegate = [[AppDelegate alloc] init];
-        [application setDelegate:delegate];
-        
-        [application run];
-    }
+int main(void) {
+    NSAutoreleasePool *poll = [[NSAutoreleasePool alloc] init];
+    [NSApplication sharedApplication];
+    NSUInteger win_style = NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask;
+    NSRect win_rect = NSMakeRect(100, 100, 400, 400);
+    NSWindow *win = [[NSWindow alloc] initWithContentRect:win_rect
+                                          styleMask:win_style
+                                          backing:NSBackingStoreBuffered
+                                          defer:NO];
+    [win autorelease];
+    NSWindowController * win_controller = [[NSWindowController alloc] initWithWindow:win];
+    [win_controller autorelease];
+    NSTextView * text_view = [[NSTextView alloc] initWithFrame:win_rect];
+    [text_view autorelease];
+    [win setContentView:text_view];
+    [text_view insertText:@"Hello OSX/Cocoa world!"];
+    // TODO: Create app delegate to handle system events.
+    // TODO: Create menus (especially Quit!)
+    [win orderFrontRegardless];
+    [NSApp run];
+    [pool drain];
     return 0;
 }
