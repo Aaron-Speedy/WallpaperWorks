f28b9575739ed09d624aac04fccaeb278cf2eb04
Aaron-Speedy <aaron@lspeedy.org>
2025-11-11 14:59:33 -0600

    Upload

--- src/main_osx.m
+++ src/main_osx.m
@@ -148,11 +148,11 @@ void make_win_bg(NSWindow * win) {
         BOOL success = [service registerAndReturnError:&error];
         
         if (!success) NSLog(@"Login item registration failed with error: %@", error); // TODO: show an alert or something
-    } if else (service.status == SMAppServiceStatusEnabled) {
+    } else if (service.status == SMAppServiceStatusEnabled) {
         NSLog(@"Login item is already registered.");
-    } if else (service.status == SMAppServiceStatusNotFound) {
+    } else if (service.status == SMAppServiceStatusNotFound) {
         NSLog(@"App service status is not found.");
-    } if else (service.status == SMAppServiceStatusRequiresApproval) {
+    } else if (service.status == SMAppServiceStatusRequiresApproval) {
         NSLog(@"Login item requires approval.");
         [SMAppService openSystemSettingsLoginItems];
     }
