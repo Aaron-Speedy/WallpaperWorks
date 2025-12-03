5b0c50d62320241458b55e632f0e0b49790bacee
Aaron-Speedy <aaron@lspeedy.org>
2025-12-03 09:33:06 -0600

    Upload

--- src/main_osx.m
+++ src/main_osx.m
@@ -310,7 +310,9 @@ s8 arena_printf(Arena *perm, const char *fmt, ...) {
 int main(int argc, char *argv[]) {
     Arena scratch = new_arena(10 * KiB);
 
+    NSLog(@"Outside.\n");
     if (argc == 3) {
+        NSLog(@"Inside.\n");
         u64 pid = s8_to_u64((s8) { .buf = argv[1], .len = strlen(argv[1]), });
         NSString *old_bundle = [NSString stringWithCString:argv[2] encoding:NSUTF8StringEncoding];
 
