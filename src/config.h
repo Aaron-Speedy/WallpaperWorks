57a9e83f0281f3fa4ea146b6db4fc1277d638f89
Aaron-Speedy <aaron@lspeedy.org>
2025-12-17 14:31:58 -0600

    Add config.h

---
+++ src/config.h
@@ -0,0 +1,7 @@
+const float time_x = 0.04, time_y = 0.06, // from the bottom-right
+      time_size = 0.18,
+      time_shadow_x = 0.002, time_shadow_y = 0.002;
+
+const float date_x = 0.04, date_y = time_y + 0.17, // from the bottom-right
+      date_size = 0.04,
+      date_shadow_x = 0.002, date_shadow_y = 0.002;
