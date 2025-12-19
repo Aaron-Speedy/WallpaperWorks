5d0cfd9dcb7ed75d52187d4e7feb0063245bf68f
Aaron-Speedy <aaron@lspeedy.org>
2025-12-18 22:51:31 -0600

    Read from cache

--- src/main.c
+++ src/main.c
@@ -67,7 +67,7 @@ try_downloading_another_one:
         s8 url = s8_newcat(perm, base, name);
         s8 path = s8_newcat(perm, cache_dir, name);
 
-        // img_data = s8_read_file(perm, path);
+        img_data = s8_read_file(perm, path);
 
         // File was not found
         if (img_data.len <= 0) {
