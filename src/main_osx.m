da36085be1e30f0575765bfe19a456c26b9f01ea
Aaron-Speedy <aaron@lspeedy.org>
2026-03-06 19:25:23 -0600

    Use ARC again

--- src/main_osx.m
+++ src/main_osx.m
@@ -263,7 +263,7 @@ int main(int argc, char *argv[]) {
     NSApplication *app = [NSApplication sharedApplication];
 
     AppDelegate *app_delegate = [[AppDelegate alloc] init];
-    // [app_delegate autorelease];
+    [app_delegate autorelease];
     [app setDelegate:app_delegate];
 
     NSArray<NSScreen *> *screens = [NSScreen screens];
